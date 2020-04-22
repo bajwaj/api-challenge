7fb3f3b33384493cc134e237c451842a88d35898
bajwaj <38989744+bajwaj@users.noreply.github.com>
2020-04-22 12:15:39 -0700

    Submitting finished homework

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "e713fa3b0b7a7ca17daf3264526d51a9"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCDBRGdtU-bfdZU3BIrX_1XWmY8tVyZDNw"
